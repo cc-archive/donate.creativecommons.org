830ae44cd36904f2e0494ce6a10013478d677339
Kellie Brownell <kbrownell@giantrabbit.com>
2015-08-13 12:36:21 -0700

    Changing CiviCRM JS from CJ to $ Issue #22663
    Anthony recommended that we use $ for the JS changes we are making to
    the Contribution Page.  We can use $ instead of cj because we are making
    these changes in the theme, instead of a custom module.

--- sites/all/themes/cc_zen/js/script.js
+++ sites/all/themes/cc_zen/js/script.js
@@ -18,10 +18,10 @@
   Drupal.behaviors.my_custom_behavior = {
     attach: function (context, settings) {
 
-      cj('.page-civicrm-contribute-transact .email-5-section').insertAfter('.last_name-section');
-      cj('.page-civicrm-contribute-transact .form-submit').val('Process Contribution');
-      cj('.page-civicrm-contribute-transact label[for=CIVICRM_QFID_1_20]').text('I want to contribute this amount monthly.');
-      cj('.page-civicrm-contribute-transact .is_recur-section').parent().html(cj('.is_recur-section').parent().html().replace(/every/g, ''));
+      $('.page-civicrm-contribute-transact .email-5-section').insertAfter('.last_name-section');
+      $('.page-civicrm-contribute-transact .form-submit').val('Process Contribution');
+      $('.page-civicrm-contribute-transact label[for=CIVICRM_QFID_1_20]').text('I want to contribute this amount monthly.');
+      $('.page-civicrm-contribute-transact .is_recur-section').parent().html(cj('.is_recur-section').parent().html().replace(/every/g, ''));
 
     }
   };
